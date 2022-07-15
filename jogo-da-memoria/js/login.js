b8ba5c40b407621630e825a543f63660627db656
Mitchel Inoue <107430805+mitchelinoue@users.noreply.github.com>
2022-07-15 15:04:51 -0300

    jogo da memória

---
+++ jogo-da-memoria/js/login.js
@@ -0,0 +1,34 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+
+//serve para validar o nome do jogador e liberar o button play
+const validateInput = ({target}) => {
+
+    //verifica se o nome digitado tem pelo menos 2 caracteres
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
+    }
+
+    //caso não tenha 2 caracteres ou se for apagado
+    button.setAttribute('disabled', '');
+}
+
+//serve para salvar o nome do jogador no local storage do próprio navegador
+const handleSubmit = (event) => {
+
+    //serve para que o formulário não faça o comportamento padrão de enviar os valores e recarregar a página
+    event.preventDefault();
+
+    //salva o valor do nome digitado (input.value) no parâmetro player que eu defini
+    localStorage.setItem('Player', input.value)
+
+
+}
+
+//serve para escanear pelo evento de escrever no input text
+input.addEventListener('input', validateInput);
+
+//serve para escanear pelo evento de apetar o button play
+form.addEventListener('submit', handleSubmit);
