a8bcd7978e7255cabb53f9e3fd923da8d9e71095
Mitchel Inoue <107430805+mitchelinoue@users.noreply.github.com>
2022-07-18 11:15:57 -0300

    Update game.js

--- flappy-bird/game.js
+++ flappy-bird/game.js
@@ -6,6 +6,38 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
+
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+
+    desenha() {
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0,0, canvas.width, canvas.height)
+
+        contexto.drawImage(
+            sprites, 
+            planoDeFundo.spriteX, planoDeFundo.spriteY, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+            planoDeFundo.x, planoDeFundo.y, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+        );
+
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
+    },
+};
+
+
 const chao = {
     spriteX: 0,
     spriteY: 610,
@@ -22,7 +54,7 @@ const chao = {
             chao.x, chao.y, //localização x e y no canvas (canto superior esquerdo)
             chao.largura, chao.altura, // tamanho x e y da imagem no canvas
         );
-        
+
         contexto.drawImage(
             sprites,
             chao.spriteX, chao.spriteY,
@@ -53,8 +85,11 @@ const flappyBird = {
 };
 
 function loop() {
-    flappyBird.desenha();
+    
+    planoDeFundo.desenha();
     chao.desenha();
+    flappyBird.desenha();
+    
 
     requestAnimationFrame(loop);
 }
